ab28c0fcd95fa61373bfa8bd4eae5be0b1d71886
Phillip Ingraham <ingrahamphillip@gmail.com>
2017-12-09 18:28:24 -0800

    added Add Items, might change later

--- Program4PhillipAIngraham.cpp
+++ Program4PhillipAIngraham.cpp
@@ -32,15 +32,32 @@ public:
 
 	int Hash(string key) {
 		int index = 0;
-		for (int i = 0; i < key.length; i++) {
+		for (int i = 0; i < key.length; i++) { //(number ascii value * position in string[added as many as letters there are in the string]) % tableSize
 			index = (int)key[i] * (i + 1);
 		}
 		index = index % tableSize;
 		return index;
-
 	}
 
-	void addItem(string key, int num) {
+	void addItems(string key, int num) {
+		int index = Hash(key);
 
+		if (hash[index]->number == NULL && hash[index]->varName == "empty") {
+			hash[index]->number = num;
+			hash[index]->varName = key;
+		}
+
+		else {
+			node* head = hash[index];
+			node* temp = new node;
+			temp->number = num;
+			temp->varName = key;
+			temp->next = NULL;
+
+			while (head->next != NULL)
+				head = head->next;
+
+			head->next = temp;
+		}
 	}
 };
